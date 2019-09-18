ad88641e290c2175342d0483b26054c07fc2f8dc
vanthang10a9 <levanthang510@gmail.com>
2019-09-18 19:50:50 +0700

    + tao seekbar custom , + them su kien thay doi gia tri seekbar

--- app/src/main/java/com/example/seekbar/MainActivity.java
+++ app/src/main/java/com/example/seekbar/MainActivity.java
@@ -1,13 +1,18 @@
 package com.example.seekbar;
 
 import androidx.appcompat.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SeekBar mSeekBar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mSeekBar = (SeekBar) findViewById(R.id.seekbar);
+
     }
 }

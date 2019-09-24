3fc2c9121968140c682b3cb3d49ae080310e3fbe
vanthang10a9 <levanthang510@gmail.com>
2019-09-24 15:32:34 +0700

    + Thêm nút điền giá trị progress va add sự kiện vào nút

--- app/src/main/java/com/example/seekbar/MainActivity.java
+++ app/src/main/java/com/example/seekbar/MainActivity.java
@@ -2,17 +2,90 @@ package com.example.seekbar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RatingBar;
 import android.widget.SeekBar;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
     private SeekBar mSeekBar;
+    private TextView mTextView;
+    private Button mButtonIncrease;
+    private Button mButtonReduce;
+    private Button mButtonNhap;
+
+    private RatingBar mRatingBar;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
         mSeekBar = (SeekBar) findViewById(R.id.seekbar);
+        mTextView = (TextView) findViewById(R.id.textView);
+        mButtonReduce = (Button) findViewById(R.id.buttonGiam);
+        mButtonIncrease = (Button) findViewById(R.id.buttonTang);
+        mButtonNhap = (Button) findViewById(R.id.buttonNhap);
+        mRatingBar = (RatingBar) findViewById(R.id.ratingBar);
+        mRatingBar.setStepSize(2);
 
+        mSeekBar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
+            @Override
+            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
+                mTextView.setText(String.valueOf(mSeekBar.getProgress()));
+            }
+
+            @Override
+            public void onStartTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(), "Start change seekbar", Toast.LENGTH_SHORT).show();
+            }
+
+            @Override
+            public void onStopTrackingTouch(SeekBar seekBar) {
+                Toast.makeText(getApplicationContext(), "Stop change seekbar", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        mButtonReduce.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSeekBar.setProgress(mSeekBar.getProgress()-1);
+            }
+        });
+        mButtonIncrease.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mSeekBar.setProgress(mSeekBar.getProgress()+1);
+            }
+        });
+
+
+        mButtonNhap.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if( !mButtonNhap.getText().equals("Ok")){
+                    EditText mTextView = (EditText) findViewById(R.id.textView);
+                    mTextView.setText("");
+                    mTextView.setSelection(mTextView.getText().length());
+                    mTextView.requestFocus();
+                    mButtonNhap.setText("Ok");
+                }else{
+                    try{
+                        int temp = Integer.parseInt(mTextView.getText().toString());
+                        mButtonNhap.setText("Re-input");
+                        mSeekBar.setProgress(temp);
+                    }catch (Exception e){
+                        Toast.makeText(getApplicationContext(), "The input data must be a number!", Toast.LENGTH_SHORT).show();
+                    }
+                }
+            }
+        });
     }
 }
--- gradlew.bat
+++ gradlew.bat
@@ -1,69 +1,84 @@
+@if "%DEBUG%" == "" @echo off
+@rem ##########################################################################
+@rem
+@rem  Gradle startup script for Windows
+@rem
+@rem ##########################################################################
 
-package com.example.progressbar;
+@rem Set local scope for the variables with windows NT shell
+if "%OS%"=="Windows_NT" setlocal
 
-import androidx.annotation.Nullable;
-import androidx.appcompat.app.AppCompatActivity;
+set DIRNAME=%~dp0
+if "%DIRNAME%" == "" set DIRNAME=.
+set APP_BASE_NAME=%~n0
+set APP_HOME=%DIRNAME%
 
-import android.app.ProgressDialog;
-import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.ProgressBar;
-import android.widget.TextView;
+@rem Add default JVM options here. You can also use JAVA_OPTS and GRADLE_OPTS to pass JVM options to this script.
+set DEFAULT_JVM_OPTS=
 
-public class MainActivity extends AppCompatActivity {
+@rem Find java.exe
+if defined JAVA_HOME goto findJavaFromJavaHome
 
-    private ProgressBar mProgressBar;
-    private TextView mTextViewTienDo;
-    private Thread thread;
+set JAVA_EXE=java.exe
+%JAVA_EXE% -version >NUL 2>&1
+if "%ERRORLEVEL%" == "0" goto init
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+echo.
+echo ERROR: JAVA_HOME is not set and no 'java' command could be found in your PATH.
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
 
-        mTextViewTienDo = (TextView) findViewById(R.id.textviewTienDo);
-        mProgressBar = (ProgressBar) findViewById(R.id.progress_horizontal);
-        thread = new Thread() {
-            @Override
-            public void run() {
-                try {
-                    synchronized (this) {
-                        wait(5000);
+goto fail
 
-                        runOnUiThread(new Runnable() {
-                            @Override
-                            public void run() {
-                                for( int i=0; i<=100; i++){
-                                    mProgressBar.setProgress(i);
-                                    mTextViewTienDo.setText(String.valueOf(i) + "%");
-                                }
-                            }
-                        });
+:findJavaFromJavaHome
+set JAVA_HOME=%JAVA_HOME:"=%
+set JAVA_EXE=%JAVA_HOME%/bin/java.exe
 
-                    }
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-            }
+if exist "%JAVA_EXE%" goto init
 
-            ;
-        };
-        thread.start();
-    }
+echo.
+echo ERROR: JAVA_HOME is set to an invalid directory: %JAVA_HOME%
+echo.
+echo Please set the JAVA_HOME variable in your environment to match the
+echo location of your Java installation.
 
-//
-//    public class runProgressBar extends Thread {
-//        @Override
-//        public void run() {
-//           runOnUiThread(new Runnable() {
-//                @Override
-//                public void run() {
-//                    for (int i = 0; i < 100; i+=1) {
-//                        try {
-//                            Thread.sleep(500);
-//                        } catch (InterruptedException e) {
-//                            e.printStackTrace();
-//                        }
-//                        mProgressBar.setProgress(i);
-//                        mTextVi
+goto fail
+
+:init
+@rem Get command-line arguments, handling Windows variants
+
+if not "%OS%" == "Windows_NT" goto win9xME_args
+
+:win9xME_args
+@rem Slurp the command line arguments.
+set CMD_LINE_ARGS=
+set _SKIP=2
+
+:win9xME_args_slurp
+if "x%~1" == "x" goto execute
+
+set CMD_LINE_ARGS=%*
+
+:execute
+@rem Setup the command line
+
+set CLASSPATH=%APP_HOME%\gradle\wrapper\gradle-wrapper.jar
+
+@rem Execute Gradle
+"%JAVA_EXE%" %DEFAULT_JVM_OPTS% %JAVA_OPTS% %GRADLE_OPTS% "-Dorg.gradle.appname=%APP_BASE_NAME%" -classpath "%CLASSPATH%" org.gradle.wrapper.GradleWrapperMain %CMD_LINE_ARGS%
+
+:end
+@rem End local scope for the variables with windows NT shell
+if "%ERRORLEVEL%"=="0" goto mainEnd
+
+:fail
+rem Set variable GRADLE_EXIT_CONSOLE if you need the _script_ return code instead of
+rem the _cmd.exe /c_ return code!
+if  not "" == "%GRADLE_EXIT_CONSOLE%" exit 1
+exit /b 1
+
+:mainEnd
+if "%OS%"=="Windows_NT" endlocal
+
+:omega
